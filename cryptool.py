311d1873ff8a24013b6be7dc925718b53d31841a
Marcos César Vargas Magaña <esocram@gmail.com>
2020-03-25 02:05:43 -0600

    Code documentation
    Finished the code documentation.

--- cryptool.py
+++ cryptool.py
@@ -20,57 +20,135 @@ import pdb
 
 
 class Cryptool:
+	'''
+	Class to simplify the encryption and decryption of data presented either in text strings, raw bytes or files.
+	'''
 	def __init__(self):
-		# The default encoding when we encode/decode between 'str' and 'bytes'.
+		# The default text encoding when we encode/decode between 'str' and 'bytes'.
 		self.encoding = "utf8"
-		# The hash algorithm used in all the procedures (length in bytes can be obtained with '.digest_size').
+		# The hash algorithm used in all the procedures 
+		# The output length in Bytes can be obtained with '.digest_size'.
 		self.hash_algorithm = hashes.SHA3_512()
-		# The length of the keys used to encrypt/decrypt.
+		# The length in Bytes of the keys used to encrypt/decrypt.
 		self.key_length = 32
 		# The number of iterations when using a Key Derivation Function (KDF).
 		self.kdf_iterations = 1000000
 		# Function to generate salts (as raw bytes).
 		self.salt = lambda: os.urandom(self.key_length)
+		# The file extension used to save encrypted files.
+		self.extension = "ctl"
 	#
-
-	# This function should be used to obtain the default Key Derivation Function (KDF).
+	
 	def getKDF(self, salt, length=None):
+		'''
+		Defines a Key Derivation Function (KDF). This is useful to generate secure cryptographic keys from
+		a simpler phrase like a password. Never generate two keys using the same pair (salt, password).
+		This function is used in all the functions that use a KDF.
+		Parameters:
+		- salt [bytes]: raw bytes string of size 'self.key_length', use 'self.salt()' to obtain a secure salt..
+		- length [int]: all the keys generated with the returned KDF will have this size in Bytes.
+		Return:
+		- A key derivation function.
+		Usage:
+		 - kdf = self.getKDF(salt, length)
+		   key = kdf.derive(password)
+		'''
 		if length is None: length = self.key_length
 		return PBKDF2HMAC(algorithm=self.hash_algorithm, length=length, salt=salt, iterations=self.kdf_iterations, backend=default_backend())
 	#
 
 	def getCipherAlgorithm(self, key):
+		'''
+		Defines a cipher algorithm used in a Cipher Object to encrypt data using the given key.
+		This function is used in all the functions that use an encryption algorithm.
+		Parameters:
+		- key [bytes]: raw bytes string of size 'self.key_length' bytes used to encrypt the data.
+		Return:
+		- The encryption algorithm.
+		Usage:
+		- alg = self.getCipherAlgorithm(key)
+		  cipher = Cipher(alg, mode, backend=default_backend()).encryptor()
+		  decipher = Cipher(alg, mode, backend=default_backend()).decryptor()
+		'''
 		return algorithms.AES(key)
 	#
 
-	# If the message is not a 'bytes' object then it is assumed to be an 'str' object.
 	def msgToBytes(self, msg, enc=None):
+		'''
+		The cryptographic methods work on raw bytes. In order to simplify the usage of
+		text strings, this function checks if the message is already in raw bytes and
+		if not, then it is assumed to be a text string and encodes to raw bytes.
+		Parameters:
+		- msg [byes/str]: the message to safely convert to raw bytes.
+		- enc [srt]: the encoding used to encode string objects, de default is 'self.encoding'.
+		Return:
+		- [bytes]: The raw bytes string.
+		Usage:
+		- msg = msgToBytes(msg)
+		- msg = msgToBytes(msg, "cp1252")
+		'''
 		if enc == None: enc = self.encoding
 		return msg if type(msg) == bytes else msg.encode(enc)
 	#
 
-	# Returns the hash as raw bytes.
 	def getHash(self, msg):
+		'''
+		Generate a secure hash (no key used) of the given message. The hash algorithm is defined
+		in 'self.hash_algorithm'. The resulting hash will have size 'self.hash_algorithm.digest_size'
+		in bytes.
+		Parameters:
+		msg [bytes/str]: message to hash.
+		Return:
+		[bytes]: the raw bytes hash.
+		Usage:
+		- hash = self.getHash(msg)
+		'''
 		msg = self.msgToBytes(msg)
 		hasher = hashes.Hash(self.hash_algorithm, backend=default_backend())
 		hasher.update(msg)
 		return hasher.finalize()
 	#
 
-	# Returns the key and the used salt as raw bytes. You can specify the 'length' of the key in Bytes.
-	# The returned key is not for storage.
 	def getCredentialsFromPassword(self, password, length=None, salt=None):
+		'''
+		Creates a credentials card derived from the given 'password'. The credentials include a 
+		cryptograhic key of the given 'length' and the salt used to generate the key from the 
+		password. An optional 'salt' will can be provided to recover previously generated credentials.
+		Remember that in order to generate the same password we need the same pair password-salt.
+		Parameters:
+		- password [bytes/str]: the password to derive the key.
+		- length [int]: the lenght of the derived key, in bytes. The default is 'self.key_length'.
+		- salt [bytes]: the salt used to generate the key. If not give, a new one will be generated.
+		Return:
+		[dict]: the credentials in the form of a dictionary with entries:
+		- "key" [bytes]: the derived cryptographic key.
+		- "salt" [bytes]: the salt used in combination with the password to generatet the key.
+		Usage:
+		- cred = self.getCredentialsFromPassword(password)
+		  old_cred = self.getCredentialsFromPassword(password, cred["salt"])
+		'''
 		password = self.msgToBytes(password)
 		if salt is None: salt = self.salt()
 		kdf = self.getKDF(salt, length)
 		return {"key":kdf.derive(password), "salt":salt}
 	#
 
-	# Output: ||b"\x80"||   salt   ||time||          iv               ||                   secret                         ||       signature
-	# Sizes:  ||   1   ||key_length|| 8  ||cipher algorithm block size||padded msg multiple of cipher algorithm block size||digest size of hash algorithm
-	# Only gets signed the parts between the [[ and ]].
-	# We include the time just in case we want to do something with it like data expiration.
 	def encryptMsg(self, msg, password):
+		'''
+		Encrypts the given message using the given password. The encrypted data is autenticated via a MAC
+		signature. The result will be given in the form of an encryption card containing relevant information 
+		about the encryption procedure and the result.
+		Prameters:
+		- msg [bytes/str]: the message to encrypt.
+		- password [bytes/str]: the password used for the encryption.
+		Return:
+		[dict]: The encryption card in the form of a dictionary, with the following entries:
+		- "status" [str]: "OK" if everyhing went well, otherwise an error message.
+		- "signed_data" [bytes]: the encrypted message.
+		Usage:
+		- enc_card = self.encryptMsg(msg, password)
+		  if enc_card["status"] == "OK": ... do something with enc_card["signed_data"] ...
+		'''
 		msg = self.msgToBytes(msg)
 		# Generat keys for encryption and one for autentication at once derived from the same password.
 		credentials = self.getCredentialsFromPassword(password, length=self.key_length + self.hash_algorithm.digest_size)
@@ -96,11 +174,30 @@ class Cryptool:
 		signer.update(data)
 		hmac = signer.finalize()
 		
-		return data + hmac
+		# data: ||b"\x80"||   salt   ||time||          iv               ||                   secret                         ||       signature             ||
+		# size: ||   1   ||key_length|| 8  ||cipher algorithm block size||padded msg multiple of cipher algorithm block size||digest size of hash algorithm||
+		# We include the time just in case we want to do something with it like expiration time.
+		return {"status":"OK", "signed_data":data + hmac}
 	#
 	
-		
-	def decryptMsg(self, signed_data, password):
+	def decryptMsg(self, enc_card, password):
+		'''
+		Attempts to decrypt the data given in the encryption card, with the given password.
+		If the encryted data is corrupted or the password is wrong, then an "invalid or 
+		corrupted data" error will be returned. An encryption card is generated with the 
+		'self.encryptMsg' method. Message authentication will be verified. The result will 
+		be given in the form of a decryption card containing relevant information about the 
+		decryption procedure and the result.
+		Parameters:
+		- enc_card [dict]: the card with the encrypted data.
+		- password [bytes/str]: the password to use in the decryption.
+		Return:
+		[dict]: The decryption card in the form of a dictionary, with the following entries:
+		- "status" [str]: "OK" if everyhing went well, otherwise an error message.
+		- "msg" [bytes]: the dectypted message.
+		- "enc_time" [struct_time]: the time when the message was encrypted.
+		'''
+		signed_data = enc_card["signed_data"]
 		# Validate the first byte of the signed_data.
 		if signed_data[0] != 0x80:
 			return {"status":"Error: invalid or corrupted data"}
@@ -112,7 +209,7 @@ class Cryptool:
 		pos = 1
 		salt = data[pos:pos + self.key_length]
 		pos += self.key_length
-		enc_time = struct.unpack(">Q", data[pos:pos + 8])
+		enc_time = struct.unpack(">Q", data[pos:pos + 8])[0]
 		pos += 8
 		
 		# Obtain the credentials.
@@ -128,7 +225,7 @@ class Cryptool:
 		except InvalidSignature:
 			return {"status":"Error: invalid or corrupted data"}
 		
-		# The block cipher algorithm.
+		# The cipher algorithm.
 		algorithm = self.getCipherAlgorithm(key_enc)
 
 		# Get the other parts of the data.
@@ -156,15 +253,76 @@ class Cryptool:
 		
 		return {"status":"OK", "msg":msg, "enc_time":time.localtime(enc_time)}
 	#
+
+	def encryptFile(self, password, input_path, output_name=None):
+		'''
+		Encripts the file given in 'input_path' and saves the resulting encrypted data
+		to the file with path name given in 'output_name' but appending the extension
+		'self.extension'.
+		Parameters:
+		- password [bytes/str]: the password used for the encryption.
+		- input_path [str]: the absolute/relative path to the target file.
+		- output_name [str]: the extension 'self.extension' will be appended to this path name.
+		  The default output name is equal to 'input_path'.
+		Return:
+		[dict]: relevant data about the procedure, in the form of a dictionray with the entries:
+		- "status" [str]: "OK" if everyhing went well, otherwise an error message.
+		Usage:
+		- res = self.encryptFile(password, "input/file", "output/name")
+		'''
+		# Default ouput file name.
+		if output_name is None: output_name = input_path
+
+		# Load the file as raw bytes.
+		file = open(input_path, "rb")
+		msg = file.read()
+		file.close()
+
+		# Encrypt the file bytes.
+		enc_card = self.encryptMsg(msg, password)
+		if enc_card["status"] != "OK":
+			return {"status":enc_card["status"]}
+		
+		# Save the encrypted data to a file with extension 'self.extension'.
+		file = open("%s.%s" % (output_name, self.extension), "wb")
+		file.write(enc_card["signed_data"])
+		file.close()
+
+		return {"status":"OK"}
+	#
+
+	def decryptFile(self, password, input_path, output_path=None):
+		# Separate the input path in the path name and the file extension.
+		file_name, file_extension = os.path.splitext(input_path)
+		# Default output file path.
+		if output_path == None:
+			if file_extension[1:] == self.extension:
+				output_path = file_name
+			else: return {"status":"Error: if the file has no extension '.%s' then 'output_path' must be given" % (self.extension)}
+		
+		# Load the encrypted file as raw bytes.
+		file = open(input_path, "rb")
+		signed_data = file.read()
+		file.close()
+
+		# Decrypt the file bytes.
+		decrypted = self.decryptMsg(signed_data, password)
+		if decrypted["status"] != "OK":
+			return {"status":enc_card["status"]}
+
+		# Save the decrypted data to a file.
+		file = open(output_path, "wb")
+		file.write(decrypted["msg"])
+		file.close()
+
+		return {"status":"OK"}
+	#
 #
 
 if __name__ == "__main__":
 	ct = Cryptool()
-	#k = Fernet.generate_key()
-	#x = ct.getCredentialsFromPassword("Jamás")
-	k = ct.getCredentialsFromPassword("Hola", 3)
-	s = ct.encryptMsg("Hola Jamas", "hola")
-	p = ct.decryptMsg(s, "hola")
+	#ct.encryptFile("Wallpapers.zip", "ZXCVBNM")
+	#ct.decryptFile("Wallpapers.zip.ctl", "ZXCVBNM")
 #
 
 '''
@@ -177,7 +335,4 @@ base64.b64decode(bytes)	: base64 representation -> bytes. (if there are padding 
 
 bytes.fromhex(hex)		: hex_str -> bytes.
 bytes.hex()				: bytes -> hex_str.
-
-Fernet.generate_key()	: generates rantom 32-Byte/256-bit key.
-os.urandom(bytes_size)	: random unpredictable bytes-string of the given size.
 '''
