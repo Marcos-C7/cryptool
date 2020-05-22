c48238bfc9bd2025ff8a4392b1413310f85e7847
Marcos César Vargas Magaña <esocram@gmail.com>
2020-05-22 17:15:49 -0500

    Provide installer for pip
    Provide an installer for pip, in the folder "dist\".

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ setup(	name='cryptool',
 		description='Simplified interface for encrypting and decrypting: raw bytes, text, files and directories.',
 		author='Marcos C. V. M.',
 		url='https://github.com/Marcos-C7/cryptool',
-		packages=['cryptool'],
-		package_dir={'cryptool': ''},
+		packages=['cryptool'],			# Name of the package
+		package_dir={'cryptool': ''},	# Path of the package root (current working directory in our case)
 		classifiers=['Development Status :: 3 - Alpha',
 					'License :: Freely Distributable',
 					'Programming Language :: Python :: 3',
 					'Topic :: Security :: Cryptography'],
 		keywords=['cryptography']
-     )
+	)
