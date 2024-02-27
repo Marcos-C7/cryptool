a8d9daf8b55452e10c4d17eab73a4d404f14458a
Marcos C. Vargas M <esocram@gmail.com>
2024-02-27 10:40:11 -0600

    Release 0.1.1
    - Updated the requeriments of `cryptography` because of vulneravilities.
    - Stop tracking distribution packages.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(	name='cryptool',
-		version='0.1.0',
+		version='0.1.1',
 		description='Simplified interface for encrypting and decrypting: raw bytes, text, files and directories.',
 		author='Marcos C. V. M.',
 		url='https://github.com/Marcos-C7/cryptool',
